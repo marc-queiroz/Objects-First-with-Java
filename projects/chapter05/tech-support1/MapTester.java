20590fdf24a8bff0bd73f39b2ee8dd786bba371f
EL <neonachronism@gmail.com>
2012-07-02 12:21:02 -0400

    Added blank TokenizerTest to practice using a tokenizer

--- projects/chapter05/tech-support1/MapTester.java
+++ projects/chapter05/tech-support1/MapTester.java
@@ -1,3 +1,4 @@
+ import java.util.HashMap;
 
 /**
  * Write a description of class MapTester here.
@@ -8,15 +9,17 @@
 public class MapTester
 {
     // instance variables - replace the example below with your own
-    private int x;
-
+   
+    HashMap<String,String> phoneBook = new HashMap<String,String>();
+    
+    
     /**
      * Constructor for objects of class MapTester
      */
     public MapTester()
     {
         // initialise instance variables
-        x = 0;
+        
     }
 
     /**
@@ -25,9 +28,22 @@ public class MapTester
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public int sampleMethod(int y)
+    public void enterNumber(String name, String number)
     {
         // put your code here
-        return x + y;
+        phoneBook.put(name, number);
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void lookupNumber(String name)
+    {
+        // put your code here
+        String number = phoneBook.get(name);
+        System.out.println(name + ": " + number);
     }
 }
