6b280b06fc1e6b930a3e9c80d2c53047990c2120
EL <neonachronism@gmail.com>
2012-07-03 18:14:49 -0400

    same as previous

---
+++ projects/chapter06/diary-prototype/Main.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void makeAppointment(int day,int time,int duration,String description)
+    {
+        // put your code here
+        Day createdDay = new Day(day);
+        Appointment appointment = new Appointment(description,duration);
+        
+        createdDay.makeAppointment(time,appointment);
+        
+        
+    }
+}
